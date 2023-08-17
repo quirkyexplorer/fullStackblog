40016fdee81a8dae672efb2aab30eaabd01a81d5
quirkexplorer <quirkyexplorer@gmail.com>
2023-08-17 16:50:44 -0700

    comasela

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
+const Blog = require("./models/blog")
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const logger = require("./utils/logger");
 require("dotenv").config();
-
+const mongoose = require('mongoose')
 mongoose.set("strictQuery", false);
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number
+// })
 
-const Blog = mongoose.model('Blog', blogSchema)
+// const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl,{ 
