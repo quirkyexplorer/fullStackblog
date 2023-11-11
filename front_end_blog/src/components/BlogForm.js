97b7fcb99b2b76a8dd97d6d52514f25b1a428e4c
quirkexplorer <quirkyexplorer@gmail.com>
2023-11-10 23:06:58 -0700

    'refactored Blogform.js'

--- front_end_blog/src/components/BlogForm.js
+++ front_end_blog/src/components/BlogForm.js
@@ -1,19 +1,27 @@
-export default function BlogForm(props) {
+export default function BlogForm({
+  createBlog,
+  handleTitleChange,
+  handleAuthorChange,
+  handleUrlChange,
+  title,
+  author,
+  url
+  }) {
 
     return (
         <div>
             <h3>New Blog</h3>
-            <form onSubmit={props.createBlog}>
+            <form onSubmit={createBlog}>
                 <div>
-                    title: <input placeholder="title" value={props.title} onChange={props.handleTitleChange}/>
+                    title: <input placeholder="title" value={title} onChange={handleTitleChange}/>
                 </div>
                 <br></br>
                 <div>
-                    author: <input placeholder="author" value={props.author} onChange={props.handleAuthorChange}/>
+                    author: <input placeholder="author" value={author} onChange={handleAuthorChange}/>
                 </div>
                 <br></br>
                 <div>
-                    Url: <input placeholder="url" value={props.url} onChange={props.handleUrlChange}/>
+                    Url: <input placeholder="url" value={url} onChange={handleUrlChange}/>
                 </div>
                 <div>
                     <button type="submit">save</button>
